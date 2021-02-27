7025f5eb6f309c45d7ab1d9d57c0e08ef4966f73
朱辉 <imharryzhu@gmail.com>
2021-02-27 15:01:55 +0800

    使用Time.unscaledDeltaTime计算显示每秒帧数

---
+++ Assets/Scripts/FrameRateCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FrameRateCounter : MonoBehaviour
+{
+    TextMeshProUGUI display;
+
+    void Awake()
+    {
+        display = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        // Time.deltaTime 是前一帧到当前帧的时间，它受时间停止、慢放影响
+        // 所以应该使用Time.unscaledDeltaTime获取实际帧间隔时间
+        float frameDuration = Time.unscaledDeltaTime;
+
+
+        display.SetText("FPS\n{0:0}\n000\n000", 1 / frameDuration);
+    }
+
+}
