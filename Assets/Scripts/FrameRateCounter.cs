621b611af5f2303b373e569532aeef830c0f357b
harry <imharryzhu@gmail.com>
2021-02-28 15:49:22 +0800

    基础教程04-FPS值的计算改为时间区间内的平均值表示

--- Assets/Scripts/FrameRateCounter.cs
+++ Assets/Scripts/FrameRateCounter.cs
@@ -5,7 +5,12 @@ using TMPro;
 
 public class FrameRateCounter : MonoBehaviour
 {
+    [SerializeField, Range(0.1f, 2f)]
+    float sampleDuration = 1f;
+
     TextMeshProUGUI display;
+
+    float duration = 0, frame = 0;
 
     void Awake()
     {
@@ -17,9 +22,15 @@ public class FrameRateCounter : MonoBehaviour
         // Time.deltaTime 是前一帧到当前帧的时间，它受时间停止、慢放影响
         // 所以应该使用Time.unscaledDeltaTime获取实际帧间隔时间
         float frameDuration = Time.unscaledDeltaTime;
-
-
-        display.SetText("FPS\n{0:0}\n000\n000", 1 / frameDuration);
+        duration += Time.unscaledDeltaTime;
+        frame++;
+        if (duration >= sampleDuration)
+        {
+            display.SetText("FPS\n{0:0}\n000\n000", frame / duration);
+            frame = 0;
+            duration = 0;
+        }
+        
     }
 
 }
