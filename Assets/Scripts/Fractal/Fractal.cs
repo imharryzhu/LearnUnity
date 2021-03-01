5d1395e609054b38e053e06385d4bdb7edd3ce2c
朱辉 <imharryzhu@gmail.com>
2021-03-01 21:50:11 +0800

    基础教程06-创建Sierpiński 三角形

--- Assets/Scripts/Fractal/Fractal.cs
+++ Assets/Scripts/Fractal/Fractal.cs
@@ -8,6 +8,7 @@ public class Fractal : MonoBehaviour
     [SerializeField, Range(1, 8)]
     int depth = 4;
 
+    // 使用Start而非Awake，因为Start在创建下一帧执行
     void Start()
     {
         // name属性设置该物体名称
@@ -18,8 +19,26 @@ public class Fractal : MonoBehaviour
             return;
         }
 
-        // 用Start而不用Awake，是因为Start在下一帧执行，Awake是实例化当帧执行
-        Fractal child = Instantiate<Fractal>(this);
+        var childA = CreateChild(Vector3.up, Quaternion.identity);
+        var childB = CreateChild(Vector3.right, Quaternion.Euler(0, 0, -90));
+        var childC = CreateChild(Vector3.left, Quaternion.Euler(0, 0, 90));
+        var childD = CreateChild(Vector3.forward, Quaternion.Euler(90, 0, 0));
+        var childE = CreateChild(Vector3.back, Quaternion.Euler(-90, 0, 0));
+        
+        childA.transform.SetParent(transform, false);
+        childB.transform.SetParent(transform, false);
+        childC.transform.SetParent(transform, false);
+        childD.transform.SetParent(transform, false);
+        childE.transform.SetParent(transform, false);
+    }
+
+    Fractal CreateChild(Vector3 direction, Quaternion rotation)
+    {
+        Fractal child = Instantiate(this);
         child.depth -= 1;
+        child.transform.localPosition = 0.75f * direction;
+        child.transform.localScale = 0.5f * Vector3.one;
+        child.transform.localRotation = rotation;
+        return child;
     }
 }
