897e6ce6de51d18941d899ab4eb65de9178f3a9e
harry <imharryzhu@gmail.com>
2021-03-03 00:24:22 +0800

    基础教程06-有点秃，不过感觉开始进入状态了

--- Assets/Scripts/Fractal/Fractal.cs
+++ Assets/Scripts/Fractal/Fractal.cs
@@ -30,10 +30,13 @@ public class Fractal : MonoBehaviour
         Quaternion.Euler(-90, 0, 0)
     };
 
+    // 分型的基本信息
     struct FractalPart
     {
         public Vector3 direction, worldPosition;
         public Quaternion rotation, worldRotation;
+        // 为了避免用四元数计算，保存欧拉角用于后续计算
+        public float angle;
     }
 
     // 存储part的二维数组
@@ -42,14 +45,24 @@ public class Fractal : MonoBehaviour
     // 用于渲染的变换矩阵数组
     Matrix4x4[][] matrices;
 
-    void Awake()
+    // shader属性参数
+    static readonly int matricesId = Shader.PropertyToID("_Matrices");
+
+    ComputeBuffer[] matricesBuffers;
+
+    // 将Awake改为OnEnable，为了在OnDisable时释放缓冲区
+    void OnEnable()
     {
         parts = new FractalPart[depth][];
         matrices = new Matrix4x4[depth][];
+        matricesBuffers = new ComputeBuffer[depth];
+        // 4x4的矩阵有16个字节
+        int stride = 16 * 4;
         for (int i = 0, len = 1; i < depth; ++i, len *= 5)
         {
             parts[i] = new FractalPart[len];
             matrices[i] = new Matrix4x4[len];
+            matricesBuffers[i] = new ComputeBuffer(len, stride);
         }
 
         /*       长度
@@ -60,16 +73,7 @@ public class Fractal : MonoBehaviour
          */
 
         // 创建根节点
-        var rootPart = CreatePart(0);
-        parts[0][0] = rootPart;
-        // 根节点的变换矩阵
-        // 平移-旋转-缩放 Translation-Rotation-Scale
-        var mat4 = Matrix4x4.TRS(
-           rootPart.worldPosition,
-           rootPart.worldRotation,
-           Vector3.one
-        );
-        matrices[0][0] = mat4;
+        parts[0][0] = CreatePart(0);
 
         // 创建所有子节点
         for (int li = 1; li < depth; li++)
@@ -86,15 +90,47 @@ public class Fractal : MonoBehaviour
         }
     }
 
+    // 需要Disable时释放缓冲区
+    void OnDisable()
+    {
+        foreach (var buf in matricesBuffers)
+        {
+            buf.Release();
+        }
+        parts = null;
+        matrices = null;
+        matricesBuffers = null;
+    }
+
+    // 调用条件： 脚本加载时、Inspector界面数值修改时
+    void OnValidate()
+    {
+        if (parts != null && enabled)
+        {
+            OnDisable();
+            OnEnable();
+        }
+    }
+
     void Update()
     {
-        Quaternion deltaRotation = Quaternion.Euler(0, 22.5f * Time.deltaTime, 0);
-
+        // 不对四元数进行递增操作，float值的微量不精确累加会导致四元数不合法！
+        // Quaternion deltaRotation = Quaternion.Euler(0, 22.5f * Time.deltaTime, 0);
+        float angleDelta = 22.5f * Time.deltaTime;
         // 旋转根节点
         var rootPart = parts[0][0];
-        rootPart.rotation *= deltaRotation;
+        // rootPart.rotation *= deltaRotation;
+        rootPart.angle += angleDelta;
         rootPart.worldRotation = rootPart.rotation;
         parts[0][0] = rootPart;
+        // 根节点的变换矩阵
+        // 平移-旋转-缩放 Translation-Rotation-Scale
+        var mat4 = Matrix4x4.TRS(
+           rootPart.worldPosition,
+           rootPart.worldRotation,
+           Vector3.one
+        );
+        matrices[0][0] = mat4;
 
         float scale = 1;
         for (int li = 1; li < depth; li++)
@@ -104,17 +140,33 @@ public class Fractal : MonoBehaviour
             var parentParts = parts[li - 1];
             var levelParts = parts[li];
             var levelMat4 = matrices[li];
+            Matrix4x4[] levelMatrices = matrices[li];
             for (int fpi = 0; fpi < levelParts.Length; fpi++)
             {
                 var parentPart = parentParts[fpi / 5];
                 var part = levelParts[fpi];
-                part.rotation *= deltaRotation;
+                part.rotation *= Quaternion.Euler(0, part.angle, 0); ;
                 part.worldRotation = parentPart.worldRotation * part.rotation;
                 part.worldPosition = parentPart.worldPosition +
                     parentPart.worldRotation *
                     (1.5f * scale * part.direction);
                 levelParts[fpi] = part;
+
+                levelMatrices[fpi] = Matrix4x4.TRS(
+                    part.worldPosition,
+                    part.worldRotation,
+                    scale * Vector3.one
+                );
             }
+        }
+
+        var bounds = new Bounds(Vector3.zero, 3 * Vector3.one);
+        for (int i = 0; i < matricesBuffers.Length; i++)
+        {
+            var buffer = matricesBuffers[i];
+            buffer.SetData(matrices[i]);
+            material.SetBuffer(matricesId, buffer);
+            Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, buffer.count);
         }
     }
 
---
+++ Assets/Shader/Fractal/FractalGPU.shader
@@ -0,0 +1,46 @@
+﻿Shader "Fractal/Fractal Surface GPU"
+{
+    Properties {
+        _Color ("Albedo", Color) = (1, 1, 1, 1)
+        _Smoothness ("Smoothness", Range(0, 1)) = .5
+    }
+    SubShader {
+        CGPROGRAM
+        #pragma surface ConfigureSurface Standard fullforwardshadows addshadow
+        #pragma instancing_options procedural:ConfigureProcedural
+        #pragma editor_sync_compilation
+        #pragma target 4.5
+
+
+#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+        StructuredBuffer<float4x4> _Matrices;
+#endif
+        void ConfigureProcedural() {
+#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+            unity_ObjectToWorld = _Matrices[unity_InstanceID];
+#endif
+        }
+
+        void ShaderGraphFunction_float(float3 In, out float3 Out) {
+            Out = In;
+        }
+
+        void ShaderGraphFunction_half(float3 In, out float3 Out) {
+            Out = In;
+        }
+
+        struct Input {
+            float3 worldPos;
+        };
+
+        float4 _Color;
+        float _Smoothness;
+
+        void ConfigureSurface(Input input, inout SurfaceOutputStandard surface) {
+            surface.Albedo = _Color.rgb;
+            surface.Smoothness = _Smoothness;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
