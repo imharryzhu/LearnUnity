9607d12ce7b56cf3b0785687f989064ce78603f5
harry <imharryzhu@gmail.com>
2021-03-04 00:08:54 +0800

    物体运动01-添加小球根据玩家摇杆操作移动产生拖尾效果

---
+++ Assets/Scripts/PlayerControlledMotion/MoveSphere.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour
+{
+    void Update()
+    {
+        Vector2 playerInput = new Vector2(0, 0);
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        transform.localPosition = new Vector3(playerInput.x, 0, playerInput.y);
+    }
+}
