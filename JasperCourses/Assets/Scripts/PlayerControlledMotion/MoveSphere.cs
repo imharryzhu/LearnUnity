0eb25691aaf344e7ae33e0272bf468f5f14fcfed
harry <imharryzhu@gmail.com>
2021-03-04 23:30:24 +0800

    物体运动01-限制向量模长使方向切换更加平滑、在Inspector窗口增加属性的Tip

--- JasperCourses/Assets/Scripts/PlayerControlledMotion/MoveSphere.cs
+++ JasperCourses/Assets/Scripts/PlayerControlledMotion/MoveSphere.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class MoveSphere : MonoBehaviour
 {
+    [SerializeField, Range(1f, 100f), Tooltip("最大移动速度")]
+    float maxSpeed = 10f;
+
     void Update()
     {
-        Vector2 playerInput = new Vector2(0, 0);
+        Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
-        transform.localPosition = new Vector3(playerInput.x, 0, playerInput.y);
+
+        // 将向量模长限制在 1
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        Vector3 displacement = maxSpeed * Time.deltaTime * new Vector3(playerInput.x, 0f, playerInput.y);
+        transform.localPosition += displacement;
     }
 }
