4d3aa1d083622f658d26c37f4995ef44cc93e095
harry <imharryzhu@gmail.com>
2021-03-20 23:24:12 +0800

    物体运动04-当物体移动超过聚焦范围后，相机才平滑跟随物体

--- JasperCourses/Assets/Scripts/MoveSphere/OrbitCamera.cs
+++ JasperCourses/Assets/Scripts/MoveSphere/OrbitCamera.cs
@@ -13,15 +13,27 @@ public class OrbitCamera : MonoBehaviour
     [SerializeField, Range(1f, 20f), Tooltip("距离物体的围观距离")]
     float distance = 5f;
 
+    [SerializeField, Min(0f), Tooltip("物体移动超过该值时相机才会调整")]
+    float focusRadius = 1f;
+
+    // 当前相机焦点位置
+    Vector3 focusPoint;
+
+    void Awake()
+    {
+        focusPoint = focus.position;
+    }
+
     /*
+     * 更新相机位置一般都放在LateUpdate中
+     * 
      * LateUpdate在所有Update执行完在执行
      * 避免Update任务过重也避免闪帧的问题
-     * 
      */
     void LateUpdate()
     {
         // 目标世界坐标
-        Vector3 focusPoint = focus.position;
+        UpdateFocusPoint();
 
         // 相机看向的方向
         Vector3 lookDir = transform.forward;
@@ -29,4 +41,26 @@ public class OrbitCamera : MonoBehaviour
         // 将相机放在正确的位置
         transform.position = focusPoint - lookDir * distance;
     }
+
+    void UpdateFocusPoint()
+    {
+        Vector3 targetPoint = focus.position;
+        if (focusRadius > 0)
+        {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+            if (distance > focusRadius)
+            {
+                // 线性插值，平滑移动相机到焦点位置
+                focusPoint = Vector3.Lerp(
+                    targetPoint, focusPoint, focusRadius / distance);
+            }
+
+        }
+        else
+        {
+            // 将物体位置直接赋值给相机焦点位置
+            focusPoint = targetPoint;
+        }
+    }
+
 }
