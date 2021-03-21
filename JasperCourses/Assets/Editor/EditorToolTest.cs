64c00e10ad834b3318e4c9849d003265df928909
朱辉 <imharryzhu@gmail.com>
2021-03-21 16:51:53 +0800

    演示在Project窗口扩展右键菜单的功能

---
+++ JasperCourses/Assets/Editor/EditorToolTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/**
+ * 演示在Project窗口扩展右键菜单的功能
+ *  
+ */
+
+public class EditorToolTest
+{
+    [MenuItem("Assets/ToolTest/打印选中项名")]
+    static void Tool1()
+    {
+        Debug.Log(Selection.activeObject.name);
+    }
+
+    [MenuItem("Assets/ToolTest/创建/立方体")]
+    static void CreateCube()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+}
