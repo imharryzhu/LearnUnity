557746afe4827452d2da4cb9fc39cf81d94b9878
harry <imharryzhu@gmail.com>
2021-04-15 23:56:50 +0800

    对象管理01-按键创建随机立方体

---
+++ ObjectManager/Assets/Scripts/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public Transform prefab;
+    public KeyCode createKey = KeyCode.C;
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(createKey))
+        {
+            CreateObject();
+        }
+    }
+
+    void CreateObject()
+    {
+        Transform t = Instantiate(prefab);
+
+        // 在一个球体的空间内的随机一个点
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Random.Range(0.1f, 1f) * Vector3.one;
+    }
+}
